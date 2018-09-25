6188bab8d50d22d747fa96e164a698e26fe8957f
Gatlin Johnson <gatlin@niltag.net>
2018-09-25 14:54:35 -0500

    http port should be a configurable option

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { renderFile } from 'ejs';
 import { join } from 'path';
 import * as express from 'express';
+import * as jsonfile from 'jsonfile';
+
+const config = jsonfile.readFileSync('./config.json');
 
 (async () => {
     const app = await NestFactory.create(AppModule);
     app.use(express.static(join(__dirname, '../../ui/build')));
-    await app.listen(3000);
+    await app.listen(config.http_port || 3000);
 })();
