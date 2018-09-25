bcd4eb7feb89fa7de41551dac0b969fa0d658474
Gatlin Johnson <gatlin@niltag.net>
2018-09-25 14:56:29 -0500

    adding local copies of alm because I made some changes

---
+++ alm.ts
@@ -0,0 +1,408 @@
+/**
+ * @module alm
+ *
+ * The main entry point for the alm library.
+ */
+
+import { VDom, VDomType, initialDOM, diff_dom } from './vdom';
+
+/**
+ * Messages consumed by a {@link Reducer}. Messages are tagged with a type of
+ * your choosing (likely an Enum of some kind).
+ */
+export type Message<T> = {
+    'type': T;
+    data?: any;
+};
+
+/**
+ * A left fold on your application state which consumes {@link Message} values
+ * and a state object and returns a modified state object.
+ */
+export type Reducer<S, A> = (state: S, action: Message<A>) => S;
+
+/**
+ * A function to combine multiple {@link Reducer}s, each governing one piece of
+ * the total state.
+ *
+ * @param reducers - An object whose keys correspond to the top-level state
+ * keys. Each corresponding value is a {@link Reducer} for that part of the
+ * state.
+ * @return A composite reducer.
+ */
+export function makeReducer(reducers) {
+    const reducerKeys = Object.keys(reducers);
+    return (state, action) => {
+        let hasChanged = false;
+        const newState = {};
+        for (let i = 0; i < reducerKeys.length; i++) {
+            const key = reducerKeys[i];
+            const reducer = reducers[key];
+            const previousState = state[key];
+            const nextState = reducer(previousState, action);
+            newState[key] = nextState;
+            hasChanged = hasChanged || nextState !== previousState;
+        }
+        return hasChanged ? newState : state;
+    };
+}
+
+/**
+ * Alternatively asynchronous messages may be dispatched. These accept a
+ * {@link Store#dispatch} function along with a thunk returning the application
+ * state and return a {@link Message}. This allows an action to dispatch other
+ * actions.
+ */
+export type AsyncMessage<S, A> =
+    (d: (a: Message<A>) => void, s: () => S) => Message<A>;
+
+/**
+ * Manages application state using an initial state value and a {@link Reducer}.
+ */
+export class Store<S, Action> {
+
+    protected subscribers: Array<() => void>;
+
+    constructor(protected state: S, private reducer: Reducer<S, Action>) {
+        this.subscribers = [];
+    }
+
+    /**
+     * Dispatch a {@link Message} to the {@link Reducer}.
+     *
+     * @param action - The message describing which action to perform on the
+     * state. Can either be a message or a continuation which asks for the
+     * dispatch function before producing the message.
+     * @return The store.
+     */
+    public dispatch(action: Message<Action> | AsyncMessage<S, Action>): this {
+        this.state = this.reducer(
+            this.state,
+            typeof action === 'function'
+                ? action(this.dispatch.bind(this), this.getState.bind(this))
+                : action
+        );
+        this.subscribers.forEach(update => { update(); });
+        return this;
+    }
+
+    /**
+     * The Store keeps an array of functions to call whenever the state changes.
+     *
+     * @param subscriber - A function to call on each state change.
+     * @return A function which may be used to unsubscribe the function.
+     */
+    public subscribe(subscriber) {
+        this.subscribers.push(subscriber);
+        // hand back a way to unsubscribe
+        return () => {
+            const idx = this.subscribers.indexOf(subscriber);
+            this.subscribers.splice(idx, 1);
+        };
+    }
+
+    /**
+     * A getter for the state.
+     *
+     * @return The sealed state.
+     */
+    public getState() {
+        return Object.seal(this.state);
+    }
+}
+
+/**
+ * An object made available to every {@link Component}.
+ *
+ * A Context contains a pointer to the application {@link Store} as well as a
+ * function which can be used to register event handlers.
+ */
+export interface Context<S, A> {
+    store: Store<S, A>;
+    handle: (e: HTMLElement, h: Object) => void;
+};
+
+/**
+ * The foundation of a user interface. A View converts a {@link Context} into a
+ * {@link VDom}.
+ */
+export type View<S, A> = (c: Context<S, A>) => VDom;
+
+function flatten<T>(ary: Array<Array<T>>): Array<T> {
+    return ary.reduce((a, b) => [...a, ...b], []);
+}
+
+/**
+ * Convenience function for constructing a {@link View}.
+ *
+ * @param ctor - A string or a {@link Component}. A string value should be an
+ * HTML tag.
+ * @param props - Properties to set on the HTML element or the component.
+ * @param _children - An array of child {@link View}s.
+ * @return A new {@link View}.
+ */
+export function el<S, A>(ctor, props: any = {}, ..._children): View<S, A> {
+    return ctx => {
+        props = props === null ? {} : props;
+
+        // Grab the event handlers
+        let eventHandlers = {};
+        if (props.on) {
+            eventHandlers = props.on;
+            delete props.on;
+        }
+
+        // Since `class` is a reserved identifier in ECMAScript we call it
+        // `className` in the JSX code.
+        if (props.className) {
+            props['class'] = props.className;
+            delete props['className'];
+        }
+
+        if (props['forId']) {
+            props['for'] = props['forId'];
+            delete props['forId'];
+        }
+
+        // A ref is a callback which is given the real actual DOM node once it
+        // has rendered.
+        if (props.ref) {
+            eventHandlers['ref'] = props['ref'];
+            delete props['ref'];
+        }
+        _children = Array.isArray(_children) && Array.isArray(_children[0])
+            ? _children[0]
+            : _children;
+        const children: Array<View<S, A>> = _children
+            ? _children
+                .filter(child => typeof child !== 'undefined')
+                .map((child, idx) => {
+                    if (!child || child instanceof Array) {
+                        return null;
+                    }
+                    if (typeof child === 'string') {
+                        return new VDom(child, [], VDomType.Text);
+                    }
+                    if (typeof child === 'function') {
+                        return child(ctx);
+                    }
+                    return child;
+                })
+                .filter(child => child !== null)
+            : [];
+
+        // Give a component access to its children via props
+        //props['children'] = children;
+
+        // This function will be called when/if the VDom is ever actually
+        // rendered into a real DOM node and will be used to set the appropriate
+        // event handlers.
+        const handler = e => { ctx.handle(e, eventHandlers); };
+
+        // If the constructor is a string it refers to an HTML element name and
+        // so we can create a VDom node for it. Otherwise it must be a component
+        // function which will, ultimately, return a VDom.
+        const view = typeof ctor === 'string'
+            ? new VDom({
+                tag: ctor,
+                attrs: props
+            }, children, VDomType.Node, handler)
+            : ctor({ ...props, children })(ctx);
+
+        return view;
+    };
+}
+
+/**
+ * A Component is a pure function from an argument to a {@link View}.
+ */
+export type Component<Props, S, A> = (props: Props) => View<S, A>;
+
+/**
+ * Wraps around browser events.
+ */
+export class AlmEvent {
+    private readonly raw: any;
+    private readonly classes: Array<string>;
+    private readonly id: string;
+    private readonly value: any;
+
+    constructor(evt) {
+        this.raw = evt;
+        this.classes = evt.target.className.trim().split(/\s+/g) || [];
+        this.id = evt.target.id || '';
+        this.value = evt.target.value;
+    }
+
+    public hasClass(klass) {
+        return (this.classes.indexOf(klass) !== -1);
+    }
+
+    public getClasses() {
+        return this.classes;
+    }
+
+    public getId() {
+        return this.id;
+    }
+
+    public getValue() {
+        return this.value;
+    }
+
+    public getRaw() {
+        return this.raw;
+    }
+
+    public class_in_ancestry(klass: string) {
+        let result = null;
+        let done = false;
+        let elem = this.raw.target;
+
+        while (!done) {
+            if (!elem.className) {
+                done = true;
+                break;
+            }
+
+            let klasses = elem.className.trim().split(/\s+/g) || [];
+            if (klasses.indexOf(klass) !== -1) {
+                result = elem;
+                done = true;
+            }
+            else if (elem.parentNode) {
+                elem = elem.parentNode;
+            }
+
+            else {
+                done = true;
+            }
+
+        }
+        return result;
+    }
+}
+
+/**
+ * The configuration object required to create an {@link Alm} app.
+ */
+export type AppConfig<State, Action> = {
+    model: State;
+    update: Reducer<State, Action>;
+    view: View<State, Action>,
+    eventRoot?: HTMLElement | Document | string;
+    domRoot?: HTMLElement | string;
+};
+
+/**
+ * The application lifecycle manager. It listens for top-level events and binds
+ * to part of the DOM to display the user interface.
+ */
+export class Alm<State, Action> {
+    public readonly store: Store<State, Action>;
+    private view: View<State, Action>;
+    private eventRoot: HTMLElement | Document;
+    private domRoot: HTMLElement;
+    private events: object;
+    private gensymnumber: number = 0;
+
+    constructor(cfg: AppConfig<State, Action>) {
+        this.store = new Store(cfg.model, cfg.update);
+
+        this.eventRoot = typeof cfg.eventRoot === 'string'
+            ? document.getElementById(cfg.eventRoot)
+            : typeof cfg.eventRoot === 'undefined'
+                ? document
+                : cfg.eventRoot;
+
+        this.domRoot = typeof cfg.domRoot === 'string'
+            ? document.getElementById(cfg.domRoot)
+            : cfg.domRoot;
+
+        this.view = cfg.view;
+        this.events = {};
+    }
+
+    public start() {
+        this.events = {};
+        let store = this.store;
+        let handle = (e, handlers) => {
+            window.setTimeout(() => {
+                let eId;
+                if (e.hasAttribute('data-alm-id')) {
+                    eId = e.getAttribute('data-alm-id');
+                } else {
+                    eId = this.gensym();
+                    e.setAttribute('data-alm-id', eId);
+                }
+
+                if (handlers.ref) {
+                    handlers.ref(e);
+                    delete handlers['ref'];
+                }
+
+                for (let evtName in handlers) {
+                    if (!(evtName in this.events)) {
+                        this.events[evtName] = {};
+                        this.registerEvent(evtName, this.handleEvent);
+                    }
+                    this.events[evtName][eId] = handlers[evtName];
+                }
+
+                return () => {
+                    for (let evtName in handlers) {
+                        delete this.events[evtName][eId];
+                    }
+                };
+            }, 0);
+        };
+        let context: Context<State, Action> = { store, handle };
+        let vtree = this.view(context);
+        initialDOM(this.domRoot, vtree);
+
+        this.store.subscribe(() => {
+            const updated = this.view(context);
+            diff_dom(this.domRoot, vtree, updated);
+            vtree = updated;
+        });
+    }
+
+    private handleEvent = evt => {
+        const evtName = evt.type;
+        if (this.events[evtName]) {
+            if (evt.target.hasAttribute('data-alm-id')) {
+                const almId = evt.target.getAttribute('data-alm-id');
+                if (this.events[evtName][almId]) {
+                    this.events[evtName][almId](new AlmEvent(evt));
+                }
+            }
+        }
+    };
+
+    private gensym() {
+        return 'node-' + (this.gensymnumber++).toString();
+    }
+
+    /**
+     * Register an event listener for the specified event.
+     */
+    private registerEvent(evtName, cb) {
+        this.eventRoot.addEventListener(evtName, cb, true);
+    }
+}
+
+/**
+ * Connect the store to a pure component.
+ */
+export function connect(mapState = null, mapDispatch = null) {
+    return component => (props = {}) => ctx => {
+        const store = ctx.store;
+        const state = store.getState();
+        const mappedState = mapState ? mapState(state) : {};
+        const mappedDispatch = mapDispatch
+            ? mapDispatch(store.dispatch.bind(store))
+            : {};
+        const finalProps = { ...props, ...mappedState, ...mappedDispatch };
+        return component(finalProps)(ctx);
+    };
+}
---
+++ vdom.ts
@@ -0,0 +1,322 @@
+/**
+ * @module vdom
+ *
+ * This module started off as a clone of:
+ * {@link https://github.com/Matt-Esch/virtual-dom}
+ *
+ * However at this point, despite retaining some nomenclature, it's an entirely
+ * different algorithm.
+ *
+ * Rather than actually compute a set of patches and then apply them in two
+ * phases, this algorithm computes patches and then applies them immediately.
+ */
+
+export type Attrs = {
+    [key: string]: string;
+};
+
+export type El = {
+    tag: string;
+    attrs: Attrs;
+};
+
+export enum VDomType {
+    Text,
+    Node
+};
+
+/**
+ * A rose tree representing DOM elements. Can represent either an element node
+ * or a text node.
+ *
+ * Because VDom is lighter weight than actual DOM elements an efficient diff
+ * procedure can be used to compare old and new trees and determine what needs
+ * to be done to the actual DOM.
+ *
+ * The {@link VDom#key} property is used to determine equality. If a `key`
+ * attribute is provided, it will be used. If there is not one, then `id` will
+ * be used. Failing that the tag name will be used. If this is a text node, the
+ * text itself will be used. I'm open to other possibilities, especially
+ * regarding that last one.
+ */
+export class VDom {
+    public content: El;
+    public children: Array<VDom>;
+    public treeType: VDomType;
+    public key: string;
+    public onCreate: (e: HTMLElement) => (() => void) | null;
+    public onDestroy: () => void | null;
+
+    constructor(content, children, treeType, handler = null) {
+        this.content = content;
+        this.children = children;
+        this.treeType = treeType;
+        this.onCreate = handler;
+        this.onDestroy = null;
+
+        /* There must be a key */
+        if (treeType === VDomType.Node) {
+            if ('key' in this.content.attrs) {
+                this.key = this.content.attrs.key;
+                delete this.content.attrs.key;
+            }
+            else if ('id' in this.content.attrs) {
+                this.key = this.content.attrs.id;
+            }
+            else {
+                this.key = this.content.tag;
+            }
+        }
+        else {
+            this.key = 'text-node';
+        }
+    }
+
+    public setChildren(children: Array<VDom>): this {
+        this.children = children;
+        return this;
+    }
+
+    /** Equality based on the key. */
+    eq(other: VDom): boolean {
+        if (!other) {
+            return false;
+        }
+        return (this.key === other.key);
+    }
+}
+
+/** Constructs an actual DOM node from a {@link VDom}. */
+function makeDOMNode(tree): any {
+    if (tree === null) { return null; }
+    if (tree.treeType === VDomType.Text) {
+        return document.createTextNode(tree.content);
+    }
+    const el = document.createElement(tree.content.tag);
+
+    for (let key in tree.content.attrs) {
+        if (tree.content.attrs[key] !== null) {
+            el.setAttribute(key, tree.content.attrs[key]);
+        }
+    }
+
+    for (let i = 0; i < tree.children.length; i++) {
+        const child = makeDOMNode(tree.children[i]);
+        el.appendChild(child);
+    }
+
+    tree.onDestroy = tree.onCreate(el);
+
+    return el;
+}
+
+/** Constructs an initial DOM from a {@link VDom}. */
+export function initialDOM(domRoot, tree) {
+    const root = domRoot;
+    const domTree = makeDOMNode(tree);
+    while (root.firstChild) {
+        root.removeChild(root.firstChild);
+    }
+    root.appendChild(domTree);
+}
+
+/**
+ * A simple enum representing three kinds of array edit operations.
+ */
+export enum Op {
+    Merge,
+    Delete,
+    Insert
+};
+
+// Type alias for readability.
+export type Eq<T> = (a: T, b: T) => boolean;
+
+/**
+ * Computes an array of edit operations allowing the first argument to be
+ * transformed into the second argument.
+ *
+ * @param a - The original array
+ * @param b - The the desired array
+ * @param eq - An equality testing function for elements in the arrays.
+ * @return An array of {@link Op} values.
+ */
+export function diff_array<T>(a: Array<T>, b: Array<T>, eq: Eq<any>) {
+
+    if (!a.length) {
+        return b.map(c => [Op.Insert, null, c]);
+    }
+
+    if (!b.length) {
+        return a.map(c => [Op.Delete, c, null]);
+    }
+
+    const m = a.length + 1;
+    const n = b.length + 1;
+
+    const d = new Array(m * n);
+    const moves = [];
+
+    for (let i = 0; i < m; i++) {
+        d[i * n] = i;
+    }
+
+    for (let j = 0; j < n; j++) {
+        d[j] = j;
+    }
+
+    for (let j = 1; j < n; j++) {
+        for (let i = 1; i < m; i++) {
+            if (eq(a[i - 1], b[j - 1])) {
+                d[i * n + j] = d[(i - 1) * n + (j - 1)];
+            }
+            else {
+                d[i * n + j] = Math.min(
+                    d[(i - 1) * n + j],
+                    d[i * n + (j - 1)]
+                ) + 1;
+            }
+        }
+    }
+
+    let i = m - 1;
+    let j = n - 1;
+
+    while (i > 0 && j > 0) {
+        if (eq(a[i - 1], b[j - 1])) {
+            i--;
+            j--;
+            moves.unshift([Op.Merge, a[i], b[j]]);
+        }
+        else {
+            if (d[i * n + (j - 1)] < d[(i - 1) * n + j]) {
+                j--;
+                moves.unshift([Op.Insert, null, b[j]]);
+            }
+            else {
+                i--;
+                moves.unshift([Op.Delete, a[i], null]);
+            }
+        }
+    }
+
+    if (i > 0) {
+        while (i > 0) {
+            moves.unshift([Op.Delete, a[i--], null]);
+        }
+    }
+
+    if (j > 0 && i === 0) {
+        while (j > 0) {
+            moves.unshift([Op.Insert, null, b[--j]]);
+        }
+    }
+
+    return moves;
+}
+
+/**
+ * The name is a little misleading. This takes an old and a current
+ * {@link VDom}, the parent node of the one the old tree represents,
+ * and an (optional) index into that parent's childNodes array.
+ *
+ * If either of the trees is null or undefined this triggers DOM node creation
+ * or destruction.
+ *
+ * If both are nodes then attributes are reconciled followed by children.
+ *
+ * Otherwise the new tree simply overwrites the old one.
+ *
+ * While this does not perform a perfect tree diff it doesn't need to and
+ * performance is (probably) the better for it. In typical cases a DOM node will
+ * add or remove a few children at once, and the grandchildren will not need to
+ * be recovered from their parents. Meaning starting from the root node we can
+ * treat this as a list diff problem for the children and then, once children
+ * are paired up, we can recurse on them.
+ */
+export function diff_dom(parent, a, b, index = 0) {
+
+    if (typeof b === 'undefined' || b === null) {
+        if (parent.childNodes[index]) {
+            if (parent.childNodes[index].onDestroy) {
+                parent.childNodes[index].onDestroy();
+            }
+            parent.removeChild(parent.childNodes[index]);
+        }
+        return;
+    }
+
+    if (typeof a === 'undefined' || a === null) {
+        parent.insertBefore(
+            makeDOMNode(b),
+            parent.childNodes[index]);
+        return;
+    }
+
+    if (b.treeType === VDomType.Node) {
+        if (a.treeType === VDomType.Node) {
+            if (a.content.tag === b.content.tag) {
+
+                // contend with attributes. only necessary changes.
+                let dom = parent.childNodes[index];
+
+                for (let attr in a.content.attrs) {
+                    if (!(attr in b.content.attrs)) {
+                        dom.removeAttribute(attr);
+                        delete dom[attr];
+                    }
+                }
+
+                for (let attr in b.content.attrs) {
+                    const v = b.content.attrs[attr];
+                    if (!(attr in a.content.attrs) ||
+                        v !== a.content.attrs[attr]) {
+                        dom[attr] = v;
+                        dom.setAttribute(attr, v);
+                    }
+                }
+
+                window.setTimeout(() => {
+                    if (dom.hasAttribute('value')) {
+                        dom.value = dom.getAttribute('value');
+                    }
+                }, 0);
+
+                // contend with the children.
+                const moves = diff_array(
+                    a.children,
+                    b.children,
+                    (a, b) => {
+                        if (typeof a === 'undefined')
+                            return false;
+                        return a.eq(b);
+                    }
+                );
+
+                let domIndex = 0;
+                for (let i = 0; i < moves.length; i++) {
+                    const move = moves[i];
+                    diff_dom(
+                        parent.childNodes[index],
+                        move[1],
+                        move[2],
+                        domIndex
+                    );
+                    if (move[0] !== Op.Delete) {
+                        domIndex++;
+                    }
+                }
+            }
+        }
+
+    } else {
+        // different types of nodes, `b` is a text node, or they have different
+        // tags. in all cases just replace the DOM element.
+        if (parent.childNodes[index].onDestroy) {
+            parent.childNodes[index].onDestroy();
+        }
+        parent.replaceChild(
+            makeDOMNode(b),
+            parent.childNodes[index]);
+    }
+}
